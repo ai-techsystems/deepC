75483a81e04acfc3f08915ce846e6bba1b3bf883
Gunjan Nandy <gunjan.nandy1@gmail.com>
2019-09-04 18:07:52 +0530

    Updated interface automation

--- swig/dnnc_api.cpp
+++ swig/dnnc_api.cpp
@@ -8,7 +8,7 @@
 // "License"); you may not use this file except in compliance
 // with the License.You may obtain a copy of the License at
 //
-//   http://www.apache.org/licenses/LICENSE-2.0
+//	 http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing,
 // software distributed under the License is distributed on an
@@ -19,7 +19,7 @@
 //
 // This file is part of AITS DNN compiler maintained at
 // https://github.com/ai-techsystems/dnnCompiler
-//
+
 #include <Python.h>
 
 #include "core/tensor.h"
@@ -52,7 +52,6 @@
 #include "operators/LpNormalization.h"
 #include "operators/MatMul.h"
 #include "operators/MatMulInteger.h"
-#include "operators/Max.h"
 #include "operators/Mean.h"
 #include "operators/Min.h"
 #include "operators/ThresholdedRelu.h"
@@ -64,216 +63,287 @@ extern std::vector<size_t> listTupleToVector_SizeT(PyObject *);
 using namespace dnnc;
 
 tensor<float> array(size_t x, size_t y = 0, size_t z = 0, size_t w = 0) {
-  return tensor<float>(x, y, z, w, "", dnnc::INIT_RANDOM);
+	return tensor<float>(x, y, z, w, "", dnnc::INIT_RANDOM);
 }
 
 tensor<float> empty(size_t x, size_t y = 0, size_t z = 0, size_t w = 0) {
-  return tensor<float>(x, y, z, w, "", dnnc::INIT_NONE);
+	return tensor<float>(x, y, z, w, "", dnnc::INIT_NONE);
 }
 
 tensor<float> zeros(size_t x, size_t y = 0, size_t z = 0, size_t w = 0) {
-  return tensor<float>(x, y, z, w, "", dnnc::INIT_ZERO);
+	return tensor<float>(x, y, z, w, "", dnnc::INIT_ZERO);
 }
 
 tensor<float> ones(size_t x, size_t y = 0, size_t z = 0, size_t w = 0) {
-  return tensor<float>(x, y, z, w, "", dnnc::INIT_ONE);
+	return tensor<float>(x, y, z, w, "", dnnc::INIT_ONE);
 }
 
 tensor<float> random(size_t x, size_t y = 0, size_t z = 0, size_t w = 0) {
-  return tensor<float>(x, y, z, w, "", dnnc::INIT_RANDOM);
+	return tensor<float>(x, y, z, w, "", dnnc::INIT_RANDOM);
 }
 
-tensor<float> reshape(tensor<float> &tensorObj, PyObject *newShape) {
-  if (PyLong_Check(newShape)) {
-    std::vector<size_t> nShape;
-    auto newShapeMember = PyLong_AsLong(newShape);
-    nShape.push_back(static_cast<size_t>(newShapeMember));
-    if (nShape[0] <= 0)
-      throw std::logic_error("reshape integer must have positive value.\n");
-
-    tensorObj.reshape(nShape);
-  } else if (PyTuple_Check(newShape)) {
-    auto vShape = listTupleToVector_SizeT(newShape);
-    for (size_t i = 0; i < vShape.size(); i++)
-      if (vShape[i] <= 0)
-        throw std::logic_error("reshape tupel must have positive elements.\n");
-
-    tensorObj.reshape(vShape);
-  } else {
-    throw std::logic_error(
-        "reshape expect number or tupel as new shape argument.\n");
-  }
-  return tensorObj;
+tensor<float> reshape(tensor<float> &tens_obj, PyObject *newShape) {
+	if (PyLong_Check(newShape)) {
+		std::vector<size_t> nShape;
+		auto newShapeMember = PyLong_AsLong(newShape);
+		nShape.push_back(static_cast<size_t>(newShapeMember));
+		if (nShape[0] <= 0)
+			throw std::logic_error("reshape integer must have positive value.\n");
+		tensor<float> result = tens_obj.reshape(nShape);
+		return result;
+	} else if (PyTuple_Check(newShape)) {
+		auto vShape = listTupleToVector_SizeT(newShape);
+		for (size_t i = 0; i < vShape.size(); i++)
+			if (vShape[i] <= 0)
+				throw std::logic_error("reshape tupel must have positive elements.\n");
+		tensor<float> result = tens_obj.reshape(vShape);
+		return result;
+	}
+	return tens_obj;
 }
 
 tensor<float> array(PyObject *objects) {
-  if (!PyList_Check(objects))
-    throw std::logic_error("array expects list to create array.\n");
-
-  Py_ssize_t sz = PyList_Size(objects);
-  if (!sz)
-    throw std::logic_error("array expects list of non-zero size.\n");
-
-  PyObject *a_list = PyList_GetItem(objects, 0);
-  if (PyList_Check(a_list)) {
-    // 2D: dc.array(([1,2],[10,20]]);
-    // Py_ssize_t sz = PyList_Size(a_list);
-    std::vector<size_t> vDims;
-    std::vector<float> vContents;
-    for (Py_ssize_t i = 0; i < sz; i++) {
-      std::vector<float> aVec =
-          listTupleToVector_Float(PyList_GetItem(objects, i));
-      vDims.push_back(aVec.size());
-      vContents.insert(vContents.end(), aVec.begin(), aVec.end());
-    }
-    // make sure VDims are consistent.
-    for (size_t i = 1; i < vDims.size(); i++) {
-      if (vDims[0] != vDims[i])
-        throw std::logic_error(
-            "2D array expects list of lists with same size.");
-    }
-    // fill it as row major data.
-    tensor<float> result(vDims.size(), vDims[0]);
-    result.load(vContents);
-    return result;
-  } else {
-    // 1D: dc.array([2,3,4])
-    std::vector<float> aVec = listTupleToVector_Float(objects);
-    tensor<float> result(aVec.size());
-    result.load(aVec);
-    return result;
-  }
-  return tensor<float>();
+	if (!PyList_Check(objects))
+		throw std::logic_error("array expects list to create array.\n");
+	Py_ssize_t sz = PyList_Size(objects);
+	if (!sz)
+		throw std::logic_error("array expects list of non-zero size.\n");
+	PyObject *a_list = PyList_GetItem(objects, 0);
+	if (PyList_Check(a_list)) {
+		// 2D: dc.array(([1,2],[10,20]]);
+		// Py_ssize_t sz = PyList_Size(a_list);
+		std::vector<size_t> vDims;
+		std::vector<float> vContents;
+		for (Py_ssize_t i = 0; i < sz; i++) {
+			std::vector<float> aVec =
+					listTupleToVector_Float(PyList_GetItem(objects, i));
+			vDims.push_back(aVec.size());
+			vContents.insert(vContents.end(), aVec.begin(), aVec.end());
+		}
+		// make sure VDims are consistent.
+		for (size_t i = 1; i < vDims.size(); i++) {
+			if (vDims[0] != vDims[i])
+				throw std::logic_error(
+						"2D array expects list of lists with same size.");
+		}
+		// fill it as row major data.
+		tensor<float> result(vDims.size(), vDims[0]);
+		result.load(vContents);
+		return result;
+	} else {
+		// 1D: dc.array([2,3,4])
+		std::vector<float> aVec = listTupleToVector_Float(objects);
+		tensor<float> result(aVec.size());
+		result.load(aVec);
+		return result;
+	}
+	return tensor<float>();
 }
 
 tensor<float> arange(size_t stop, size_t start = 0, size_t step = 1) {
-  // swap if the range is invalid.
-  if (stop < start) {
-    size_t tmp = start;
-    start = stop;
-    stop = tmp;
-  }
-  if (stop == start)
-    throw std::logic_error("arange expects stop arg more than start.\n");
+	// swap if the range is invalid.
+	if (stop < start) {
+		size_t tmp = start;
+		start = stop;
+		stop = tmp;
+	}
+	if (stop == start)
+		throw std::logic_error("arange expects stop arg more than start.\n");
+	std::vector<float> vContents;
+	for (size_t i = start; i < stop; i = i + step)
+		vContents.push_back(static_cast<float>(i));
+	tensor<float> result(vContents.size());
+	result.load(vContents);
+	return result;
+}
 
-  std::vector<float> vContents;
-  for (size_t i = start; i < stop; i = i + step)
-    vContents.push_back(static_cast<float>(i));
-
-  tensor<float> result(vContents.size());
-  result.load(vContents);
-  return result;
+tensor<double> matmul(tensor<double> &a, tensor<double> &b) {
+	MatMul<double> op;
+	return op.compute(a, b);
 }
 
 tensor<float> matmul(tensor<float> &a, tensor<float> &b) {
-  MatMul<float> op;
-  return op.compute(a, b);
+	MatMul<float> op;
+	return op.compute(a, b);
+}
+
+tensor<int> matmul(tensor<int> &a, tensor<int> &b) {
+	MatMul<int> op;
+	return op.compute(a, b);
+}
+
+tensor<double> add(tensor<double> &a, tensor<double> &b) {
+	Add<double> op;
+	return op.compute(a, b);
 }
 
 tensor<float> add(tensor<float> &a, tensor<float> &b) {
-  Add<float> op;
-  return op.compute(a, b);
+	Add<float> op;
+	return op.compute(a, b);
 }
 
-tensor<float> dequantize_linear(tensor<float> &a, tensor<float> &b,
-                                tensor<float> &c) {
-  DequantizeLinear<float> op;
-  return op.compute(a, b, c);
+tensor<int> add(tensor<int> &a, tensor<int> &b) {
+	Add<int> op;
+	return op.compute(a, b);
+}
+
+tensor<float> dequantize_linear(tensor<float> &a, tensor<float> &b, tensor<float> &c) {
+	DequantizeLinear<float> op;
+	return op.compute(a, b, c);
+}
+
+tensor<double> div(tensor<double> &a, tensor<double> &b) {
+	Div<double> op;
+	return op.compute(a, b);
 }
 
 tensor<float> div(tensor<float> &a, tensor<float> &b) {
-  Div<float> op;
-  return op.compute(a, b);
+	Div<float> op;
+	return op.compute(a, b);
+}
+
+tensor<int> div(tensor<int> &a, tensor<int> &b) {
+	Div<int> op;
+	return op.compute(a, b);
+}
+
+tensor<double> elu(tensor<double> &a, float alpha = 1.0) {
+	Elu<double> op("localOpName", alpha);
+	return op.compute(a);
 }
 
 tensor<float> elu(tensor<float> &a, float alpha = 1.0) {
-  Elu<float> op("localOpName", alpha);
-  return op.compute(a);
+	Elu<float> op("localOpName", alpha);
+	return op.compute(a);
 }
 
-tensor<bool> equal(tensor<int> &a, tensor<int> &b) {
-  Equal<int> op;
-  return op.compute(a, b);
+tensor<bool> equal(tensor<double> &a, tensor<double> &b) {
+	Equal<double> op;
+	return op.compute(a, b);
 }
 
 tensor<bool> equal(tensor<float> &a, tensor<float> &b) {
-  Equal<float> op;
-  return op.compute(a, b);
+	Equal<float> op;
+	return op.compute(a, b);
+}
+
+tensor<bool> equal(tensor<bool> &a, tensor<bool> &b) {
+	Equal<bool> op;
+	return op.compute(a, b);
+}
+
+tensor<bool> equal(tensor<int> &a, tensor<int> &b) {
+	Equal<int> op;
+	return op.compute(a, b);
+}
+
+tensor<double> erf(tensor<double> &a) {
+	Erf<double> op;
+	return op.compute(a);
 }
 
 tensor<float> erf(tensor<float> &a) {
-  Erf<float> op;
-  return op.compute(a);
+	Erf<float> op;
+	return op.compute(a);
+}
+
+tensor<int> erf(tensor<int> &a) {
+	Erf<int> op;
+	return op.compute(a);
+}
+
+tensor<double> exp(tensor<double> &a) {
+	Exp<double> op;
+	return op.compute(a);
 }
 
 tensor<float> exp(tensor<float> &a) {
-  Exp<float> op;
-  return op.compute(a);
+	Exp<float> op;
+	return op.compute(a);
+}
+
+tensor<double> eye_like(tensor<double> &a, int k = 0) {
+	EyeLike<double> op("localOpName", k);
+	return op.compute(a);
 }
 
 tensor<float> eye_like(tensor<float> &a, int k = 0) {
-  EyeLike<float> op("localOpName", k);
-  return op.compute(a);
+	EyeLike<float> op("localOpName", k);
+	return op.compute(a);
+}
+
+tensor<int> eye_like(tensor<int> &a, int k = 0) {
+	EyeLike<int> op("localOpName", k);
+	return op.compute(a);
+}
+
+tensor<bool> eye_like(tensor<bool> &a, int k = 0) {
+	EyeLike<bool> op("localOpName", k);
+	return op.compute(a);
+}
+
+tensor<double> flatten(tensor<double> &a, int axis = 1) {
+	Flatten<double> op("localOpName", axis);
+	return op.compute(a);
 }
 
 tensor<float> flatten(tensor<float> &a, int axis = 1) {
-  Flatten<float> op("localOpName", axis);
-  return op.compute(a);
+	Flatten<float> op("localOpName", axis);
+	return op.compute(a);
+}
+
+tensor<int> flatten(tensor<int> &a, int axis = 1) {
+	Flatten<int> op("localOpName", axis);
+	return op.compute(a);
+}
+
+tensor<bool> flatten(tensor<bool> &a, int axis = 1) {
+	Flatten<bool> op("localOpName", axis);
+	return op.compute(a);
+}
+
+tensor<double> floor(tensor<double> &a) {
+	Floor<double> op;
+	return op.compute(a);
 }
 
 tensor<float> floor(tensor<float> &a) {
-  Floor<float> op;
-  return op.compute(a);
+	Floor<float> op;
+	return op.compute(a);
 }
 
-tensor<float> gemm(tensor<float> &a, tensor<float> &b, tensor<float> &c,
-                   float alpha = 1.0, float beta = 1.0, int transA = 0,
-                   int transB = 0) {
-  Gemm<float> op("localOpName", alpha, beta, transA, transB);
-  return op.compute(a, b, c);
+tensor<double> gemm(tensor<double> &a, tensor<double> &b, tensor<double> &c, float alpha = 1.0, float beta = 1.0, int transA = 0, int transB = 0) {
+	Gemm<double> op("localOpName", alpha, beta, transA, transB);
+	return op.compute(a, b, c);
 }
 
-tensor<float> thresholded_relu(tensor<float> &input) {
-  ThresholdedRelu<float> op;
-  return op.compute(input);
+tensor<float> gemm(tensor<float> &a, tensor<float> &b, tensor<float> &c, float alpha = 1.0, float beta = 1.0, int transA = 0, int transB = 0) {
+	Gemm<float> op("localOpName", alpha, beta, transA, transB);
+	return op.compute(a, b, c);
 }
 
-tensor<bool> less(tensor<float> &a, tensor<float> &b) {
-  Less<float> op;
-  return op.compute(a, b);
+tensor<int> gemm(tensor<int> &a, tensor<int> &b, tensor<int> &c, float alpha = 1.0, float beta = 1.0, int transA = 0, int transB = 0) {
+	Gemm<int> op("localOpName", alpha, beta, transA, transB);
+	return op.compute(a, b, c);
 }
 
-tensor<float> log(tensor<float> &input) {
-  Log<float> op;
-  return op.compute(input);
-}
-
-tensor<float> logsoftmax(tensor<float> &input) {
-  LogSoftmax<float> op;
-  return op.compute(input);
-}
-
-tensor<float> lpnormalization(tensor<float> &input) {
-  LpNormalization<float> op;
-  return op.compute(input);
-}
-
-tensor<int> matmulinteger(tensor<int> &a, tensor<int> &b) {
-  MatMulInteger<int> op;
-  return op.compute(a, b);
-}
-
-tensor<float> transpose(tensor<float> &a) {
-  dnnc::Transpose<float> op;
-  return op.compute(a);
-}
-tensor<float> global_average_pool(tensor<float> &input) {
+tensor<float> global_average_pool(tensor<float> &a) {
   GlobalAveragePool<float> op;
-  return op.compute(input);
+  return op.compute(a);
+}
+
+tensor<double> global_average_pool(tensor<double> &a) {
+  GlobalAveragePool<double> op;
+  return op.compute(a);
 }
 
 tensor<float> global_lp_pool(tensor<float> &a, int p = 2) {
   GlobalLpPool<float> op("localOpName", p);
+  return op.compute(a);
+}
+
+tensor<double> global_lp_pool(tensor<double> &a, int p = 2) {
+  GlobalLpPool<double> op("localOpName", p);
   return op.compute(a);
 }
 
@@ -282,8 +352,23 @@ tensor<float> global_max_pool(tensor<float> &a) {
   return op.compute(a);
 }
 
+tensor<double> global_max_pool(tensor<double> &a) {
+  GlobalMaxPool<double> op;
+  return op.compute(a);
+}
+
+tensor<bool> greater(tensor<int> &a, tensor<int> &b) {
+  Greater<int> op;
+  return op.compute(a, b);
+}
+
 tensor<bool> greater(tensor<float> &a, tensor<float> &b) {
   Greater<float> op;
+  return op.compute(a, b);
+}
+
+tensor<bool> greater(tensor<double> &a, tensor<double> &b) {
+  Greater<double> op;
   return op.compute(a, b);
 }
 
@@ -292,20 +377,80 @@ tensor<float> hardmax(tensor<float> &a, int axis = 0) {
   return op.compute(a);
 }
 
-tensor<float> hardsigmoid(tensor<float> &a, float alpha = 0.2,
-                          float beta = 0.5) {
+tensor<double> hardmax(tensor<double> &a, int axis = 0) {
+  Hardmax<double> op("localOpName", axis);
+  return op.compute(a);
+}
+
+tensor<float> hardsigmoid(tensor<float> &a, float alpha = 0.2,float beta = 0.5) {
   HardSigmoid<float> op("localOpName", alpha, beta);
   return op.compute(a);
+}
+
+tensor<double> hardsigmoid(tensor<double> &a, float alpha = 0.2,float beta = 0.5) {
+  HardSigmoid<double> op("localOpName", alpha, beta);
+  return op.compute(a);
+}
+
+tensor<bool> identity(tensor<bool> &a) {
+  Identity<bool> op;
+  return op.compute(a);
+}
+tensor<bool> instancenormalization(tensor<bool> &a, tensor<bool> &scale,tensor<bool> &B, float epsilon = 1e-5) {
+  InstanceNormalization<bool> op("localOpName", epsilon);
+  return op.compute(a, scale, B);
+  dtype = {
+    "float" : "float",
+    "double" : "double"
+  }
+}
+
+tensor<int> identity(tensor<int> &a) {
+  Identity<int> op;
+  return op.compute(a);
+}
+tensor<int> instancenormalization(tensor<int> &a, tensor<int> &scale,tensor<int> &B, float epsilon = 1e-5) {
+  InstanceNormalization<int> op("localOpName", epsilon);
+  return op.compute(a, scale, B);
+  dtype = {
+    "float" : "float",
+    "double" : "double"
+  }
 }
 
 tensor<float> identity(tensor<float> &a) {
   Identity<float> op;
   return op.compute(a);
 }
+tensor<float> instancenormalization(tensor<float> &a, tensor<float> &scale,tensor<float> &B, float epsilon = 1e-5) {
+  InstanceNormalization<float> op("localOpName", epsilon);
+  return op.compute(a, scale, B);
+  dtype = {
+    "float" : "float",
+    "double" : "double"
+  }
+}
 
-tensor<bool> isinf(tensor<float> &a, int detect_positive = 1,
-                   int detect_negative = 1) {
+tensor<double> identity(tensor<double> &a) {
+  Identity<double> op;
+  return op.compute(a);
+}
+tensor<double> instancenormalization(tensor<double> &a, tensor<double> &scale,tensor<double> &B, float epsilon = 1e-5) {
+  InstanceNormalization<double> op("localOpName", epsilon);
+  return op.compute(a, scale, B);
+  dtype = {
+    "float" : "float",
+    "double" : "double"
+  }
+}
+
+tensor<bool> isinf(tensor<float> &a, int detect_positive = 1,int detect_negative = 1) {
   IsInf<float> op("localOpName", detect_positive, detect_negative);
+  return op.compute(a);
+}
+
+tensor<bool> isinf(tensor<double> &a, int detect_positive = 1,int detect_negative = 1) {
+  IsInf<double> op("localOpName", detect_positive, detect_negative);
   return op.compute(a);
 }
 
@@ -314,33 +459,64 @@ tensor<bool> isnan(tensor<float> &a) {
   return op.compute(a);
 }
 
+tensor<bool> isnan(tensor<double> &a) {
+  IsNaN<double> op;
+  return op.compute(a);
+}
+
+tensor<float> lrn(tensor<float> &a, int size, float alpha = 0.0001,float beta = 0.75, float bias = 1.0) {
+  LRN<float> op(size, "localOpName", alpha, beta, bias);
+  return op.compute(a);
+}
+
+tensor<double> lrn(tensor<double> &a, int size, float alpha = 0.0001,float beta = 0.75, float bias = 1.0) {
+  LRN<double> op(size, "localOpName", alpha, beta, bias);
+  return op.compute(a);
+}
+
 tensor<float> leakyrelu(tensor<float> &a, float alpha = 0.01) {
   LeakyRelu<float> op("localOpName", alpha);
   return op.compute(a);
 }
 
-tensor<float> instancenormalization(tensor<float> &input, tensor<float> &scale,
-                                    tensor<float> &B, float epsilon = 1e-5) {
-  InstanceNormalization<float> op("localOpName", epsilon);
-  return op.compute(input, scale, B);
-}
-tensor<float> lrn(tensor<float> &input, int size, float alpha = 0.0001,
-                  float beta = 0.75, float bias = 1.0) {
-  LRN<float> op(size, "localOpName", alpha, beta, bias);
-  return op.compute(input);
+tensor<double> leakyrelu(tensor<double> &a, float alpha = 0.01) {
+  LeakyRelu<double> op("localOpName", alpha);
+  return op.compute(a);
 }
 
-/*tensor<float> mean(std::vector<tensor<float>> inputs) {
-  Mean<float> op;
-  return op.compute(inputs);
+tensor<float> thresholded_relu(tensor<float> &a) {
+	ThresholdedRelu<float> op;
+	return op.compute(a);
 }
 
-/tensor<float> min(std::vector<tensor<float>> inputs) {
-  Min<float> op;
-/ return op.compute(inputs);
-}*/
-
-tensor<float> max(std::vector<tensor<float>> inputs) {
-  Max<float> op;
-  return op.compute(inputs);
+tensor<bool> less(tensor<float> &a, tensor<float> &b) {
+	Less<float> op;
+	return op.compute(a, b);
 }
+
+tensor<float> log(tensor<float> &a) {
+	Log<float> op;
+	return op.compute(a);
+}
+
+tensor<float> logsoftmax(tensor<float> &a) {
+	LogSoftmax<float> op;
+	return op.compute(a);
+}
+
+tensor<float> lpnormalization(tensor<float> &a) {
+	LpNormalization<float> op;
+	return op.compute(a);
+}
+
+tensor<int> matmulinteger(tensor<int> &a, tensor<int> &b) {
+	MatMulInteger<int> op;
+	return op.compute(a, b);
+}
+
+tensor<float> transpose(tensor<float> &a) {
+	dnnc::Transpose<float> op;
+	return op.compute(a);
+}
+
+
--- swig/generator.py
+++ swig/generator.py
@@ -19,6 +19,44 @@
 # This file is part of DNN compiler maintained at
 # https://github.com/ai-techsystems/dnnCompiler
 
+import ast
+
+def check_comments(s):
+	if "/*" in s:
+		print("\nUnmatched '/*' comment syntax at:\n\n"+s[s.find("*/")-100:s.find("*/")+30])
+		return 1
+	if "*/" in s:
+		print("\nUnmatched '*/' comment syntax at: \n\n"+s[s.find("*/")-100:s.find("*/")+30])
+		return 1
+	return 0
+
+def remove_comments(s):
+	for i in range(s.count("/*")):
+		comment_block = s[s.find("/*"):s.find("*/\n")+4]   # +4 is to remove "\n" after "*/"
+		s = s.replace(comment_block,"")
+	return s
+
+def get_dtype_dictionary(s):
+	string_dict = s.replace(" ","").split("dtype=")[1].split("}")[0].replace("\n","").replace("\t","")+"}"
+	dtype = ast.literal_eval(string_dict)
+	return dtype
+
+def remove_dtype(s):
+	dtype_string = ""
+	
+	if "\tdtype" in s:
+		dtype_string = s[s.find("\tdtype"):s.find("}",s.find("dtype"))+2]
+	elif "  dtype" in s:
+		dtype_string = s[s.find("  dtype"):s.find("}",s.find("dtype"))+2]
+	else:
+		dtype_string = s[s.find("dtype"):s.find("}",s.find("dtype"))+2]
+	
+	s = s.replace("} ","}").replace(dtype_string,"")
+	return s
+
+def get_swig_extern(s):
+	s = "extern "+s.split("{")[0].replace(">","> \\\n\t\t",1).replace("tensor","dnnc::tensor")+";\n"
+	return s
 
 def main():
 	try:
@@ -30,27 +68,39 @@ def main():
 		return
 	
 	else:
-		contents = contents[:contents.rfind("}")]
-		cpp_file = contents.split("using namespace dnnc;\n\n")[0]+"using namespace dnnc;\n\n"
-		swig_extern = contents.split("#include")[0]
+		split_position = contents.find("/*##")
+		cpp_file = contents[:split_position]
+		swig_extern_file = contents.split("#include")[0]
+		contents = remove_comments(contents)
+		
+		if check_comments(contents):
+			return
 
-		for content in contents.split("using namespace dnnc;\n\ntensor")[1].split("}\n\ntensor"):
+		for content in contents[split_position:].split("\n\n"):
 
-			# if float is the dtype, then it also supports int (most of the cases)
-			if "<float>" in content:
-				for dtype in ['<float>', '<int>']:
-					temp = ("tensor"+content.replace("<float>",dtype)+"}\n\n")
-					cpp_file += temp
-					temp = ("extern "+temp.split("{")[0].replace(">","> \\\n\t\t",1).replace("tensor","dnnc::tensor")+";\n")
-					swig_extern += temp
-
-			# if int or bool is the dtype, it won't support any other dtypes (most of the cases)
-			else:
-				temp= ("tensor"+content+"}\n\n")
+			if "<output>" not in content and "<input>" not in content:
+				
+				if "dtype" in content:
+					print("input output not mentioned, try again!")
+					return
+				
+				temp = content + "\n\n"
 				cpp_file += temp
-				temp = ("extern "+temp.split("{")[0].replace(">","> \\\n\t\t",1).replace("tensor","dnnc::tensor")+";\n")
-				swig_extern += temp
+				temp = get_swig_extern(temp)
+				swig_extern_file += temp
+				continue
 
+			if "dtype" not in content:
+				print("dtype not mentioned, try again!")
+				return
+
+			dtype = get_dtype_dictionary(content)
+			content = remove_dtype(content)
+			for input, output in dtype.items():
+				temp = content.replace("input",input) .replace("output",output) + "\n\n"
+				cpp_file += temp
+				temp = get_swig_extern(temp)
+				swig_extern_file += temp
 
 		with open ("dnnc_api.cpp" ,"w") as f:
 			print("Saving 'dnnc_api.cpp'")
@@ -58,16 +108,7 @@ def main():
 		
 		with open ("dnnc_swig_externs.h" ,"w") as f:
 			print("Saving 'dnnc_swig_externs.h'")
-			f.write(swig_extern)
-
-		return
-
+			f.write(swig_extern_file)
 
 if __name__=="__main__":
-	
-	consent = input("Are you sure you want to overwrite the current 'dnnc_api.cpp'? [y/N] : " )
-	
-	if consent == "y" or consent == "Y":
-		main()
-	else:
-		print("Closing program!")
+	main()
