642c277fb9a5f6b4578e55ddadd8ce0404eaac28
Rohit Sharma <srohit@gmail.com>
2019-09-20 12:47:15 -0700

    adding slice operator and positive tests randomized to match numpy behavior.

--- include/operators/Slice.h
+++ include/operators/Slice.h
@@ -35,9 +35,223 @@ public:
 
   // bool getAttribute<int>(OPATTR attrName, int& obj) ;
 
-  void compute(void) {
-    // CHANGE return-type and args
-    // AND ADD YOUR FUNCTIONAL CODE HERE
+  tensor<T> compute(
+      tensor<T> a,             // N-D Tensor of data to extract slices from
+      tensor<DIMENSION> start, // 1-D tensor of starting indices of
+                               // corresponding axis in `axes`
+      tensor<DIMENSION> end,   // 1-D tensor of ending indices (exclusive) of
+                               // corresponding axis in `axes`
+      tensor<int> axes = NULL_TENSOR<
+          int>, // 1-D tensor of axes that `starts` and `ends` apply to.
+                // Negative value means counting dimensions from the back.
+      tensor<DIMENSION> steps =
+          NULL_TENSOR<DIMENSION>) // 1-D tensor of slice step of corresponding
+                                  // axis in `axes`. Default to 1.
+  {
+    // Process and check the arguments
+
+    std::stringstream errMsg;
+
+    DIMENSION num_axes = start.shape()[0];
+    int rank = a.rank();
+
+    if (start.rank() != 1) {
+      errMsg << "start tensor is " << start.rank()
+             << "dimensional (should be 1 dimensional)" << std::endl;
+      throw std::invalid_argument(errMsg.str().c_str());
+    }
+
+    if (end.rank() != 1) {
+      errMsg << "end tensor is " << end.rank()
+             << "dimensional (should be 1 dimensional)" << std::endl;
+      throw std::invalid_argument(errMsg.str().c_str());
+    }
+
+    if (start.shape() != end.shape()) {
+      errMsg << "start and end tensor sizes don't match (";
+      errMsg << "start tensor size = " << start.shape()[0] << ", ";
+      errMsg << "end tensor size = " << end.shape()[0] << std::endl;
+      throw std::invalid_argument(errMsg.str().c_str());
+    }
+
+    if (axes == NULL_TENSOR<int>) {
+      std::vector<DIMENSION> shape{num_axes};
+      tensor<int> default_axis(shape);
+      axes = default_axis;
+      for (size_t i = 0; i < num_axes; i++) {
+        axes(i) = i + 1;
+      }
+    }
+
+    if (steps == NULL_TENSOR<DIMENSION>) {
+      std::vector<DIMENSION> shape{num_axes};
+      tensor<DIMENSION> default_steps(shape);
+      steps = default_steps;
+      for (size_t i = 0; i < num_axes; i++) {
+        steps(i) = 1;
+      }
+    }
+
+    if (axes.rank() != 1) {
+      errMsg << "axes tensor is " << axes.rank()
+             << "dimensional (should be 1 dimensional)" << std::endl;
+      throw std::invalid_argument(errMsg.str().c_str());
+    }
+
+    if (steps.rank() != 1) {
+      errMsg << "steps tensor is " << steps.rank()
+             << "dimensional (should be 1 dimensional)" << std::endl;
+      throw std::invalid_argument(errMsg.str().c_str());
+    }
+
+    if (start.shape() != axes.shape()) {
+      errMsg << "start and axes tensor sizes don't match (";
+      errMsg << "start tensor size = " << start.shape()[0] << ", ";
+      errMsg << "axes tensor size = " << axes.shape()[0] << std::endl;
+      throw std::invalid_argument(errMsg.str().c_str());
+    }
+
+    if (start.shape() != steps.shape()) {
+      errMsg << "start and axes tensor sizes don't match (";
+      errMsg << "start tensor size = " << start.shape()[0] << ", ";
+      errMsg << "steps tensor size = " << steps.shape()[0] << std::endl;
+      throw std::invalid_argument(errMsg.str().c_str());
+    }
+
+    for (size_t i = 0; i < num_axes; i++) {
+
+      // start
+      if (start(i) >= a.shape()[i]) {
+        errMsg << "start value (" << start(i) << ") along axis " << i
+               << " is beyond the size (" << a.shape()[i]
+               << ") of input tensor along the axis" << std::endl;
+        throw std::invalid_argument(errMsg.str().c_str());
+      }
+
+      // end
+      if (end(i) > (a.shape()[i] - 1)) {
+        errMsg << "end value (" << end(i) << ") along axis " << i
+               << " is beyond the size (" << a.shape()[i]
+               << ") of input tensor along the axis" << std::endl;
+        throw std::invalid_argument(errMsg.str().c_str());
+      }
+      if ((end(i) - 1) < start(i)) {
+        errMsg << "end value (" << end(i) - 1 << ") along axis" << i
+               << " is smaller than the start value (" << start(i)
+               << ") along the axis" << std::endl;
+        throw std::invalid_argument(errMsg.str().c_str());
+      }
+
+      // axes
+      if (axes(i) < 0) {
+        if ((axes(i) + rank) < 0) {
+          errMsg << "axes value (" << axes(i) << ") along axis " << i
+                 << " is beyond the input tensor dimension" << std::endl;
+          throw std::invalid_argument(errMsg.str().c_str());
+        }
+        axes(i) = rank + axes(i);
+      }
+      if (axes(i) > rank) {
+        errMsg << "axes value (" << axes(i) << ") along axis " << i
+               << " is large than the number of dimensions of input tensor"
+               << std::endl;
+        throw std::invalid_argument(errMsg.str().c_str());
+      }
+
+      for (size_t j = i + 1; j < num_axes; j++) {
+        if (axes(i) == axes(j)) {
+          errMsg << "repeated axis value (" << axes(i) << ") at indices " << i
+                 << " and " << j << " of axes input" << std::endl;
+          throw std::invalid_argument(errMsg.str().c_str());
+        }
+      }
+
+      // steps
+    }
+
+    // Determine the shape of the result tensor
+
+    std::vector<size_t> resultShape(rank);
+    std::vector<size_t> start_index(rank);
+    std::vector<size_t> end_index(rank);
+    std::vector<size_t> step(rank);
+
+    for (int axis = 0; axis < rank; axis++) {
+
+      // determine slicing along the axis-th dimension
+      for (size_t i = 0; i < num_axes; i++) {
+        if ((axes(i) - 1) == axis) {
+          start_index[axis] = start(i);
+          end_index[axis] = end(i) - 1;
+          step[axis] = steps[i];
+          break;
+        } else {
+          start_index[axis] = 0;
+          end_index[axis] = a.shape()[axis] - 1;
+          step[axis] = 1;
+        }
+      }
+
+      resultShape[axis] =
+          (end_index[axis] - start_index[axis]) / step[axis] + 1;
+    }
+
+    // Slice now
+    tensor<T> result(resultShape);
+
+    if (rank == 1) {
+      size_t i0 = 0;
+      for (size_t _i0 = start_index[0]; _i0 <= end_index[0]; _i0 += step[0]) {
+        result(i0++) = a(_i0);
+      }
+    } else if (rank == 2) {
+      size_t i0 = 0;
+      for (size_t _i0 = start_index[0]; _i0 <= end_index[0]; _i0 += step[0]) {
+        size_t i1 = 0;
+        for (size_t _i1 = start_index[1]; _i1 <= end_index[1]; _i1 += step[1]) {
+          result(i0, i1++) = a(_i0, _i1);
+        }
+        i0++;
+      }
+    } else if (rank == 3) {
+      size_t i0 = 0;
+      for (size_t _i0 = start_index[0]; _i0 <= end_index[0]; _i0 += step[0]) {
+        size_t i1 = 0;
+        for (size_t _i1 = start_index[1]; _i1 <= end_index[1]; _i1 += step[1]) {
+          size_t i2 = 0;
+          for (size_t _i2 = start_index[2]; _i2 <= end_index[2];
+               _i2 += step[2]) {
+            result(i0, i1, i2++) = a(_i0, _i1, _i2);
+          }
+          i1++;
+        }
+        i0++;
+      }
+    } else if (rank == 4) {
+      size_t i0 = 0;
+      for (size_t _i0 = start_index[0]; _i0 <= end_index[0]; _i0 += step[0]) {
+        size_t i1 = 0;
+        for (size_t _i1 = start_index[1]; _i1 <= end_index[1]; _i1 += step[1]) {
+          size_t i2 = 0;
+          for (size_t _i2 = start_index[2]; _i2 <= end_index[2];
+               _i2 += step[2]) {
+            size_t i3 = 0;
+            for (size_t _i3 = start_index[3]; _i3 <= end_index[3];
+                 _i3 += step[3]) {
+              result(i0, i1, i2, i3++) = a(_i0, _i1, _i2, _i3);
+            }
+            i2++;
+          }
+          i1++;
+        }
+        i0++;
+      }
+    } else {
+      std::cout << "Not supported";
+      return NULL_TENSOR<T>;
+    }
+
+    return result;
   }
 };
 } // namespace dnnc
---
+++ test/swig/Slice.py
@@ -0,0 +1,177 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+# pylint: disable=invalid-name, unused-argument
+#
+# This file is part of DNN compiler maintained at
+# https://github.com/ai-techsystems/dnnCompiler
+
+import common
+
+import dnnc as dc
+import numpy as np
+import unittest
+
+class SliceTest(unittest.TestCase):
+    def setUp(self):
+        self.len = 4096
+        self.np_a = np.random.randn(self.len).astype(np.float32)
+        self.dc_a = dc.array(list(self.np_a));
+    
+
+    def test_Slice1D (self):
+        len   = self.len
+        np_a  = self.np_a
+        dc_a  = self.dc_a
+        np_start = np.random.randint(len/2)
+        np_end   = np.random.randint(len/2 + 1, len)
+        np_step  = np.random.randint(1, len-1)
+        np_axes  = 1
+        dc_start = dc.array(1).asTypeULong()
+        dc_end   = dc.array(1).asTypeULong()
+        dc_axes  = dc.array(1).asTypeInt()
+        dc_step  = dc.array(1).asTypeULong()
+        dc_start[0] = np_start
+        dc_end[0]   = np_end
+        dc_axes[0]  = np_axes  
+        dc_step[0]  = np_step 
+        npr   = np_a[np_start:np_end:np_step]
+        dcr   = dc.slice(dc_a, dc_start, dc_end, dc_axes, dc_step)
+        np.testing.assert_allclose(npr, np.array(dcr.data()).astype(np.float32),
+                rtol=1e-3, atol=1e-3)
+
+
+    def test_Slice2D (self):
+        len   = np.sqrt(self.len).astype(int)
+        np_a  = np.reshape(self.np_a, (len, len))
+        # dc_a  = dc.reshape(self.dc_a, (len, len)) $ BUG (reported bug.py) - for now using a WA
+        dc_a  = dc.reshape(self.dc_a, (64, 64))
+        np_start  = np.random.randint(len/2)
+        np_end    = np.random.randint(len/2 + 1, len)
+        np_step   = np.random.randint(1, len-1)
+        np_axes   = 1
+        np_start2 = np.random.randint(len/2)
+        np_end2   = np.random.randint(len/2 + 1, len)
+        np_step2  = np.random.randint(1, len-1)
+        np_axes2  = 2
+        dc_start  = dc.array(2).asTypeULong()
+        dc_end    = dc.array(2).asTypeULong()
+        dc_axes   = dc.array(2).asTypeInt()
+        dc_step   = dc.array(2).asTypeULong()
+        dc_start[0] = np_start
+        dc_end[0]   = np_end
+        dc_axes[0]  = np_axes  
+        dc_step[0]  = np_step 
+        dc_start[1] = np_start2
+        dc_end[1]   = np_end2
+        dc_axes[1]  = np_axes2  
+        dc_step[1]  = np_step2 
+        npr   = np_a[np_start:np_end:np_step, np_start2:np_end2:np_step2]
+        dcr   = dc.slice(dc_a, dc_start, dc_end, dc_axes, dc_step)
+        np.testing.assert_allclose(npr.flatten(), np.array(dcr.data()).astype(np.float32),
+                rtol=1e-3, atol=1e-3)
+
+
+    def test_Slice3D (self):
+        len   = np.cbrt(self.len).astype(int)
+        np_a  = np.reshape(self.np_a, (len, len, len))
+        # dc_a  = dc.reshape(self.dc_a, (len, len, len))
+        dc_a  = dc.reshape(self.dc_a, (16, 16, 16))
+        np_start  = np.random.randint(len/2)
+        np_end    = np.random.randint(len/2 + 1, len)
+        np_step   = np.random.randint(1, len-1)
+        np_axes   = 1
+        np_start2 = np.random.randint(len/2)
+        np_end2   = np.random.randint(len/2 + 1, len)
+        np_step2  = np.random.randint(1, len-1)
+        np_axes2  = 2
+        np_start3 = np.random.randint(len/2)
+        np_end3   = np.random.randint(len/2 + 1, len)
+        np_step3  = np.random.randint(1, len-1)
+        np_axes3  = 3
+        dc_start  = dc.array(3).asTypeULong()
+        dc_end    = dc.array(3).asTypeULong()
+        dc_axes   = dc.array(3).asTypeInt()
+        dc_step   = dc.array(3).asTypeULong()
+        dc_start[0] = np_start
+        dc_end[0]   = np_end
+        dc_axes[0]  = np_axes  
+        dc_step[0]  = np_step 
+        dc_start[1] = np_start2
+        dc_end[1]   = np_end2
+        dc_axes[1]  = np_axes2  
+        dc_step[1]  = np_step2 
+        dc_start[2] = np_start3
+        dc_end[2]   = np_end3
+        dc_axes[2]  = np_axes3  
+        dc_step[2]  = np_step3 
+        npr   = np_a[np_start:np_end:np_step, np_start2:np_end2:np_step2, np_start3:np_end3:np_step3]
+        dcr   = dc.slice(dc_a, dc_start, dc_end, dc_axes, dc_step)
+        np.testing.assert_allclose(npr.flatten(), np.array(dcr.data()).astype(np.float32),
+                rtol=1e-3, atol=1e-3)
+
+    def test_Slice4D (self):
+        len   = np.sqrt(np.sqrt(self.len)).astype(int)
+        np_a  = np.reshape(self.np_a, (len, len, len, len))
+        # dc_a  = dc.reshape(self.dc_a, (len, len, len))
+        dc_a  = dc.reshape(self.dc_a, (8, 8, 8, 8))
+        np_start  = np.random.randint(len/2)
+        np_end    = np.random.randint(len/2 + 1, len)
+        np_step   = np.random.randint(1, len-1)
+        np_axes   = 1
+        np_start2 = np.random.randint(len/2)
+        np_end2   = np.random.randint(len/2 + 1, len)
+        np_step2  = np.random.randint(1, len-1)
+        np_axes2  = 2
+        np_start3 = np.random.randint(len/2)
+        np_end3   = np.random.randint(len/2 + 1, len)
+        np_step3  = np.random.randint(1, len-1)
+        np_axes3  = 3
+        np_start4 = np.random.randint(len/2)
+        np_end4   = np.random.randint(len/2 + 1, len)
+        np_step4  = np.random.randint(1, len-1)
+        np_axes4  = 4
+        dc_start  = dc.array(4).asTypeULong()
+        dc_end    = dc.array(4).asTypeULong()
+        dc_axes   = dc.array(4).asTypeInt()
+        dc_step   = dc.array(4).asTypeULong()
+        dc_start[0] = np_start
+        dc_end[0]   = np_end
+        dc_axes[0]  = np_axes  
+        dc_step[0]  = np_step 
+        dc_start[1] = np_start2
+        dc_end[1]   = np_end2
+        dc_axes[1]  = np_axes2  
+        dc_step[1]  = np_step2 
+        dc_start[2] = np_start3
+        dc_end[2]   = np_end3
+        dc_axes[2]  = np_axes3  
+        dc_step[2]  = np_step3 
+        dc_start[3] = np_start4
+        dc_end[3]   = np_end4
+        dc_axes[3]  = np_axes4  
+        dc_step[3]  = np_step4 
+        npr   = np_a[np_start:np_end:np_step, np_start2:np_end2:np_step2, np_start3:np_end3:np_step3, np_start4:np_end4:np_step4]
+        dcr   = dc.slice(dc_a, dc_start, dc_end, dc_axes, dc_step)
+        np.testing.assert_allclose(npr.flatten(), np.array(dcr.data()).astype(np.float32),
+                rtol=1e-3, atol=1e-3)
+        
+    def tearDown(self):
+        return "test finished"
+
+if __name__ == '__main__':
+    unittest.main()
+
