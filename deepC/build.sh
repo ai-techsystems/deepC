2d8d776629db06ebb11583ae74e5243f47e79695
Gunjan933 <gunjan.nandy1@gmail.com>
2019-10-31 11:47:21 +0530

    make cleans dnnc_swig_externs.h, docker supports deepC

--- deepC/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd ..
-sudo docker build -t dnnc .
-sudo docker run dnnc /bin/bash -c \
-  "cd /dnnCompiler/swig && make clean && make"
--- deepC/buildDocker.py
+++ deepC/buildDocker.py
@@ -7,14 +7,14 @@ def main():
 	# Mac and Linux
 	if os.name == "posix":
 		os.system('sudo docker build -t dnnc .')
-		os.system('sudo docker run -it dnnc /bin/bash -c "cd /dnnCompiler/swig && make clean && make"')
+		os.system('sudo docker run -it dnnc /bin/bash -c "cd /dnnCompiler/deepC && make clean && make"')
 
 	# Windows
 	elif os.name == "nt":
 		os.system('docker build -t dnnc .')
 		# don't use single quotes inside command, always use duble quotes, similar problem listed below
 		# https://stackoverflow.com/questions/24673698/unexpected-eof-while-looking-for-matching-while-using-sed 
-		os.system('docker run -it dnnc /bin/bash -c "cd /dnnCompiler/swig && make clean && make"')
+		os.system('docker run -it dnnc /bin/bash -c "cd /dnnCompiler/deepC && make clean && make"')
 
 
 if __name__ == "__main__":
