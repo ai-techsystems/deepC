7d029043a1a931eb0f7ebfa93ca34fcc0930a56b
SRavit1 <sharma.ravit@gmail.com>
2019-11-30 21:43:09 -0800

    Adding first version of ReduceSum operator

--- include/operators/ReduceSum.h
+++ include/operators/ReduceSum.h
@@ -28,17 +28,164 @@
 using namespace Eigen;
 
 namespace dnnc {
-template <typename T> class ReduceSum : public baseOperator<T, T, T> {
-  //  ReduceSum attributes
+template <typename To, typename Ti> class ReduceSum : public baseOperator<To, Ti, Ti> {
+  std::vector<int> _axis = {};
+  int _keepdims = 1;
+
 public:
   ReduceSum(std::string name = "opReduceSum")
-      : baseOperator<T, T, T>(opReduceSum, name) {}
+      : baseOperator<To, Ti, Ti>(opReduceSum, name) {}
 
-  // bool getAttribute<int>(OPATTR attrName, int& obj) ;
-
-  void compute(void) {
-    // CHANGE return-type and args
-    // AND ADD YOUR FUNCTIONAL CODE HERE
+  bool getAttribute(OPATTR attrName, int &obj) override {
+    if (attrName == attr_keepdims) {
+      obj = _keepdims;
+      return true;
+    }
+    return false;
   }
+  bool setAttribute(OPATTR attrName, int obj) override {
+    if (attrName == attr_keepdims) {
+      _keepdims = obj;
+      return true;
+    }
+    return false;
+  }
+  bool getAttribute(OPATTR attrName, std::vector<int> &obj)  {
+    if (attrName == attr_axis) {
+      obj = _axis;
+      return true;
+    }
+    return false;
+  }
+  bool setAttribute(OPATTR attrName, std::vector<int> obj)  {
+    if (attrName == attr_axis) {
+      _axis = obj;
+      return true;
+    }
+    return false;
+  }
+
+  tensor<To> compute(tensor<Ti> input) {
+    int rank = input.rank();
+    std::vector<DIMENSION> shape = input.shape();
+    
+    //check that values in _axis are less than or equal to the rank
+    for (int axis : _axis) {
+      if (axis > rank-1) {
+        throw std::invalid_argument("Axis is larger than input tensor rank.");
+      }
+    }
+    std::vector<DIMENSION> new_shape = input.shape();
+    
+    //sort axes from greatest to least
+    sort(_axis.begin(), _axis.end(), std::greater<int>());
+    
+    //new shape of the tensor will be composed of the original tensor shape with values in _axis removed
+    for (int axis : _axis) {
+      new_shape.erase(new_shape.begin()+axis);
+    }
+    
+    //resulting tensor has the shape new_shape
+    tensor<To> result(new_shape);
+
+    if (rank == 1) {
+      for (size_t l0 = 0; l0 < shape[0]; l0++) {
+        std::vector<size_t> k;
+        
+        k.push_back(l0);
+
+        for (int axis : _axis) {
+          k.erase(k.begin()+axis);
+        }
+
+        result(k) += static_cast<To>(input(l0));
+      }
+    }
+    if (rank == 2) {
+      for (size_t l0 = 0; l0 < shape[0]; l0++) {
+        for (size_t l1 = 0; l1 < shape[1]; l1++) {
+          std::vector<size_t> k;
+          
+          k.push_back(l0);
+          k.push_back(l1);
+
+          for (int axis : _axis) {
+            k.erase(k.begin()+axis);
+          }
+
+          result(k) += static_cast<To>(input(l0, l1));
+        }
+      }
+    }
+    if (rank == 3) {
+      for (size_t l0 = 0; l0 < shape[0]; l0++) {
+        for (size_t l1 = 0; l1 < shape[1]; l1++) {
+          for (size_t l2 = 0; l2 < shape[2]; l2++) {
+            std::vector<size_t> k;
+            
+            k.push_back(l0);
+            k.push_back(l1);
+            k.push_back(l2);
+
+            for (int axis : _axis) {
+              k.erase(k.begin()+axis);
+            }
+
+            result(k) += static_cast<To>(input(l0, l1, l2));
+            //result(k[0], k[1]) += input(l0, l1, l2);
+          }
+        }
+      }
+    }
+    if (rank == 4) {
+      for (size_t l0 = 0; l0 < shape[0]; l0++) {
+        for (size_t l1 = 0; l1 < shape[1]; l1++) {
+          for (size_t l2 = 0; l2 < shape[2]; l2++) {
+            for (size_t l3 = 0; l3 < shape[3]; l3++) {
+              std::vector<size_t> k;
+              
+              k.push_back(l0);
+              k.push_back(l1);
+              k.push_back(l2);
+              k.push_back(l3);
+
+              for (int axis : _axis) {
+                k.erase(k.begin()+axis);
+              }
+
+              result(k) += static_cast<To>(input(l0, l1, l2, l3));
+            }
+          }
+        }
+      }
+    }
+    if (rank == 5) {
+      for (size_t l0 = 0; l0 < shape[0]; l0++) {
+        for (size_t l1 = 0; l1 < shape[1]; l1++) {
+          for (size_t l2 = 0; l2 < shape[2]; l2++) {
+            for (size_t l3 = 0; l3 < shape[3]; l3++) {
+              for (size_t l4 = 0; l4 < shape[4]; l4++) {
+                std::vector<size_t> k;
+                
+                k.push_back(l0);
+                k.push_back(l1);
+                k.push_back(l2);
+                k.push_back(l3);
+                k.push_back(l4);
+
+                for (int axis : _axis) {
+                  k.erase(k.begin()+axis);
+                }
+
+                result(k) += static_cast<To>(input(l0, l1, l2, l3, l4));
+              }
+            }
+          }
+        }
+      }
+    }
+    return result;
+  }
+    
 };
 } // namespace dnnc
--- src/operators/ReduceSum.cpp
+++ src/operators/ReduceSum.cpp
@@ -29,6 +29,25 @@ using namespace Eigen;
 #ifdef DNNC_REDUCESUM_TEST
 #include <iostream>
 int main() {
-  // ADD YOUR TEST CODE HERE
+  float f1[6] = {1., 2., 3., 4., 5., 6};
+  float f2[9] = {6., 1., 0., 5., 7., 4., 2., 3., 8};
+
+  tensor<float> tf1({2, 3});
+  tf1.load(f1);
+  tensor<float> tf2({3, 3, 1});
+  tf2.load(f2);
+
+  std::vector<int> axes1 ({1});
+  std::vector<int> axes2 ({0,2});
+
+  ReduceSum<int, float> ReduceSum("floatReduceSum");
+  ReduceSum.setAttribute(attr_keepdims, 0);
+  ReduceSum.setAttribute(attr_axis, axes1);
+
+  std::cout << ReduceSum.compute(tf1) << std::endl;
+
+  ReduceSum.setAttribute(attr_axis, axes2);
+
+  std::cout << ReduceSum.compute(tf2) << std::endl;
 }
 #endif
