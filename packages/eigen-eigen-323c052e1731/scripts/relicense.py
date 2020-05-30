89cd5e5779538b78b9f9fdcc3dbd76cc03634b64
Rohit Sharma <srohit@gmail.com>
2020-05-30 09:00:31 -0700

    print python3 formatting

--- packages/eigen-eigen-323c052e1731/scripts/relicense.py
+++ packages/eigen-eigen-323c052e1731/scripts/relicense.py
@@ -52,7 +52,7 @@ rootdir = sys.argv[1]
 for root, sub_folders, files in os.walk(rootdir):
     for basename in files:
         if basename in exclusions:
-          print 'SKIPPED', filename
+          print ('SKIPPED', filename)
           continue
         filename = os.path.join(root, basename)
         fo = file(filename)
@@ -64,6 +64,6 @@ for root, sub_folders, files in os.walk(rootdir):
           fo = file(filename, "w")
           fo.write(text)
           fo.close()
-          print 'UPDATED', filename
+          print ('UPDATED', filename)
         else:
-          print '       ', filename
+          print ('       ', filename)
